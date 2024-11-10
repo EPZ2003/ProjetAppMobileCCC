b8eed217041924d1d183e752a58f4ad97af4f7bf
EPZ2003 <enzo.polizzi.11@gmail.com>
2024-11-10 12:45:39 +0100

    A31 - A41

---
+++ app/src/main/java/com/example/mobiledevicesccc/RestTime.kt
@@ -0,0 +1,40 @@
+package com.example.mobiledevicesccc
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RestTime : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_rest_time)
+
+        val btnRTP = findViewById<Button>(R.id.btnRTP)
+        val btnRTW = findViewById<Button>(R.id.btnRTW)
+        val btnRTHP = findViewById<Button>(R.id.btnRTHP)
+
+        btnRTP.setOnClickListener {
+
+            val intent = Intent(this,Pause::class.java)
+            startActivity(intent)
+        }
+        btnRTW.setOnClickListener {
+            //TODO
+            /*Need Workout Page
+            val intent = Intent(this,WorkoutPlanning::class.java)
+            startActivity(intent*/
+        }
+        btnRTHP.setOnClickListener {
+
+            val intent = Intent(this,HomePage::class.java)
+            startActivity(intent)
+        }
+    }
+}
