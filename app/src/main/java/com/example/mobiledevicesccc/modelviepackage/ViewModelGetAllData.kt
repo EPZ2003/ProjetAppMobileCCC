c9ce1eaafbb6aef99245ee2e8b730cab9252c91f
EPZ2003 <enzo.polizzi.11@gmail.com>
2024-11-30 22:53:39 +0100

    Tuto

--- app/src/main/java/com/example/mobiledevicesccc/modelviepackage/ViewModelGetAllData.kt
+++ app/src/main/java/com/example/mobiledevicesccc/modelviepackage/ViewModelGetAllData.kt
@@ -4,10 +4,9 @@ import androidx.lifecycle.ViewModel
 import com.example.mobiledevicesccc.data.Exercise
 import com.example.mobiledevicesccc.data.ExerciseDao
 import kotlinx.coroutines.flow.Flow
-
+//3.5 tu cree le view model PAR QUERIES DAO
 class ViewModelGetAllData(private val exerciseDao: ExerciseDao) : ViewModel() {
     fun getAllData(): Flow<List<Exercise>> {
         return exerciseDao.getAllExercise()
-
     }
 }
